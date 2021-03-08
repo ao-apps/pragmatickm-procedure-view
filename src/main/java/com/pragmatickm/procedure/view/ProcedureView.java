dbee6158f20c7aa50d03ec6ae39edb5e0eb9ebd2
AO Industries, Inc <support@aoindustries.com>
2021-03-08 09:01:02 -0600

    Implemented automatic newline and indentation

--- src/main/java/com/pragmatickm/procedure/view/ProcedureView.java
+++ src/main/java/com/pragmatickm/procedure/view/ProcedureView.java
@@ -118,7 +118,7 @@ public class ProcedureView extends View {
 	public <__ extends FlowContent<__>> void doView(ServletContext servletContext, HttpServletRequest request, HttpServletResponse response, __ flow, Page page) throws ServletException, IOException, SkipPageException {
 		flow.h1__(h1 -> h1
 			.text("All Procedures in ").text(page.getTitle())
-		).nl();
+		);
 
 		ProcedureTreeImpl.writeProcedureTree(servletContext, request, response, flow, page);
 	}
