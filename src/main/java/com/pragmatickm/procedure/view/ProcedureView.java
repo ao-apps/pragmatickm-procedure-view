3197aad67fe56b82db021401e5b7a5e18b58b66a
AO Industries, Inc <support@aoindustries.com>
2021-06-05 02:21:34 -0500

    Open-source projects moved to "com.aoapps" packages

--- src/main/java/com/pragmatickm/procedure/view/ProcedureView.java
+++ src/main/java/com/pragmatickm/procedure/view/ProcedureView.java
@@ -22,7 +22,7 @@
  */
 package com.pragmatickm.procedure.view;
 
-import com.aoindustries.html.servlet.FlowContent;
+import com.aoapps.html.servlet.FlowContent;
 import com.pragmatickm.procedure.model.Procedure;
 import com.pragmatickm.procedure.servlet.impl.ProcedureTreeImpl;
 import com.semanticcms.core.model.Page;
